2f28b699bf3687e2aed0dbefc1f047b16393acde
Michel Fawaz <Michelfawaz87@gmail.com>
2022-10-04 16:35:40 +0300

    .

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xB85e35B32e2CF8903a0ad302096d7f06d4372D28";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
